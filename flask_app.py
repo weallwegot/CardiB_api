31ab277e6076e8b3d30d76990d2278153acbee74
olutosinsonuyi <tosin.sonuyi@gmail.com>
2017-07-26 21:00:39 -0400

    add data manually bc webscrape was blocked

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,10 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask_cors import CORS, cross_origin
 import json
 import requests
+import urllib2
+import lxml.html
+import random
+import urllib.request, urllib.error, urllib.parse
 
 app = Flask(__name__)
 app.secret_key = "v9rex04(_2n(e4ae?xd3xec=x1dda3bd+=1x92xe290-xf6x93Lxf3_kjdh78fu4bc"
@@ -25,8 +29,18 @@ class LyricalApi(Resource):
             artist = 'cardi-b'
         else:
             artist = json_data['artist']
-        lyric = get_random_lyric(artist)
-        return {'meta':{'code':200},'data':{'lyric':lyric}}
+        lyric, song = get_random_lyric(artist)
+        return {'meta':{'code':200},'data':{'lyric':lyric, 'song':song}}
+
+def get_random_lyric(artist_string):
+    if len(artist_string) > 0:
+        first_letter = artist_string[0]
+        base_url_for_az_lyrics = 'http://www.azlyrics.com/'
+        artist_specific_url = base_url_for_az_lyrics + first_letter + '/' + artist_string + '.html'
+        page = requests.get(artist_specific_url)
+        page_tree = lxml.html.fromstring(all_articles_page.content)
+        all_a_tags = page_tree.xpath('//html//a')
+
 
 
 
