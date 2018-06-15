d1cfe511836bd50834979700b324f72e96666436
tosin <tosin.sonuyi@gmail.com>
2018-06-14 23:03:44 -0400

    break out special values to constants

---
+++ constants.py
@@ -0,0 +1,6 @@
+"""
+Keeping track of often used constants throughtout the program.
+
+..moduleauthor: Olutosin Sonuyi <tosin.sonuyi@gmail.com>
+"""
+VALID_OPTIONS = ['james_baldwin','cardi_b','jay_z','nas','inspirational_code','kanye_west','lauryn_hill','tupac']
