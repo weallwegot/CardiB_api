994fa4b882fb43ac87f37ee532c5eb1be82f35e7
tosin <tosin.sonuyi@gmail.com>
2019-01-16 23:18:45 +0100

    remove one ls call s3fs and allow for underscores in artists lyric folder names

--- aws_version/lambda_function.py
+++ aws_version/lambda_function.py
@@ -9,10 +9,12 @@ import random
 from constants import VALID_OPTIONS, SAFE_4_WORK
 from utilities import contains_curse
 
-import boto3 
+import boto3
 s3 = boto3.client("s3")
 my_s3fs = s3fs.S3FileSystem()
 toplevel_dir = "s3://bars-api/just-lyrics/"
+
+ALL_FOLDERS_OF_DATA = my_s3fs.ls(toplevel_dir)
 
 
 def respond(err, res=None):
@@ -21,7 +23,7 @@ def respond(err, res=None):
         'body': err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin" : "*" #Required for CORS support to work
+            "Access-Control-Allow-Origin": "*"  # Required for CORS support to work
         },
     }
 
@@ -41,63 +43,60 @@ def lambda_handler(event, context):
     operation = event['httpMethod']
     if operation in operations:
         if operation == 'GET':
-        
+
             lyric, song, artist = get_random_lyric()
-            logging.debug('lyric: {}\nsong: {}\nartist: {}\n'.format(lyric,song,artist))
-            payload = {'meta':{'code':200},'data':{'lyric':lyric, 'song':song, 'author':artist}}
-            
-            return respond(err=None,res=payload)
-            
-            
+            logging.debug('lyric: {}\nsong: {}\nartist: {}\n'.format(lyric, song, artist))
+            payload = {'meta': {'code': 200}, 'data': {'lyric': lyric, 'song': song, 'author': artist}}
+
+            return respond(err=None, res=payload)
+
         elif operation == 'POST':
             json_data = json.loads(event['body'])
             if 'method' in json_data.keys() and 'category' in json_data.keys():
-                method_string = json_data['method'] # right now this is unused
-                category_list = json_data['category'] # this is the "category" of quote. could be an artist or genre or inspirational
+                method_string = json_data['method']  # right now this is unused
+                # this is the "category" of quote. could be an artist or genre or inspirational
+                category_list = json_data['category']
                 print('arguments passed: {}'.format(category_list))
                 if len(category_list) == 0:
                     category_list = None
                 lyric, song, artist = get_random_lyric(category_list)
-                if lyric == song  == artist:
+                if lyric == song == artist:
                     error_msg = 'You passed an invalid argument. Use one of the following {}'.format(VALID_OPTIONS)
                     logging.error(error_msg)
-                    payload = {'meta':{'code':400},'error':{'code':400,'message':error_msg}}
+                    payload = {'meta': {'code': 400}, 'error': {'code': 400, 'message': error_msg}}
                 else:
-                    payload = {'meta':{'code':200},'data':{'lyric':lyric, 'song':song, 'author':artist}}
+                    payload = {'meta': {'code': 200}, 'data': {'lyric': lyric, 'song': song, 'author': artist}}
             else:
-    
+
                 error_msg = 'Please include a category key in your JSON with an array specifying the type of random quote you would like.'
                 error_msg += ' Your options are as follows: \n{}'.format(VALID_OPTIONS)
                 error_msg += ' Or, use a GET request with no parameters.'
                 logging.error(error_msg)
-                payload = {'meta':{'code':400},'error':{'code':400,'message':error_msg}}
-                
-            return respond(err=None,res=payload)
-        
-        
+                payload = {'meta': {'code': 400}, 'error': {'code': 400, 'message': error_msg}}
+
+            return respond(err=None, res=payload)
+
         return respond(err=None, res=operations[operation])
-    
-    
+
     else:
         return respond(ValueError('Unsupported method "{}"'.format(operation)))
-        
-        
-        
+
+
 def get_random_lyric(category_array=[]):
     """
     if there are no arguments, we will pick something random from db/.txt files
     or if there is one argument and it is the safe for work option
     """
-    logging.debug('Category Array: '+str(category_array))
+    logging.debug('Category Array: ' + str(category_array))
     # pdb.set_trace()
     # user wants curses if safe for work not in the category array
     wants_curses = not SAFE_4_WORK in category_array
     # if safe for work is only element in array, then get any song from any artist without curse words
-    if (category_array == []) or ((len(category_array)==1) and (SAFE_4_WORK in category_array)):
+    if (category_array == []) or ((len(category_array) == 1) and (SAFE_4_WORK in category_array)):
 
-        txt_file,song,cat_folder = drill_down_and_get_file_and_song()
+        txt_file, song, cat_folder = drill_down_and_get_file_and_song()
 
-        quote_or_lyric, author = piece_necessary_info_together(txt_file,song,wants_curses)
+        quote_or_lyric, author = piece_necessary_info_together(txt_file, song, wants_curses)
 
         if not author:
             # if the author isnt determined in method above then it is the category folder name
@@ -105,17 +104,15 @@ def get_random_lyric(category_array=[]):
             author = ' '.join(cat_folder.split('_')[:-1])
             # this depends on old naming convention of 'artistname _lyrics'
             # if this returns nothing or blank string, use cat folder
-            if author in ['',' ']:
+            if author in ['', ' ']:
                 # in AWS this is returning full path, but author is just the last piece
                 author = cat_folder.split('/')[-1]
             song = song.split('/')[-1]
 
-
-
-        logging.debug('Returning author: '+author)
-        logging.debug('Cat folder was: '+cat_folder)
-        logging.debug('Returning quote or lyric'+quote_or_lyric)
-        logging.debug('Returning song'+song)
+        logging.debug('Returning author: ' + author)
+        logging.debug('Cat folder was: ' + cat_folder)
+        logging.debug('Returning quote or lyric' + quote_or_lyric)
+        logging.debug('Returning song' + song)
 
         return quote_or_lyric, song, author
     else:
@@ -128,39 +125,45 @@ def get_random_lyric(category_array=[]):
         if len(valid_options_passed_in) == 0:
             error_msg = 'You passed an invalid argument. Use one of the following: {}'.format(VALID_OPTIONS)
             logging.error("Passed Invalid Args Message: {}".format(error_msg))
-            return '','',''
+            return '', '', ''
         else:
             if not wants_curses:
                 # remove safe for work so it doesnt get picked in the random author selection
                 valid_options_passed_in.remove(SAFE_4_WORK)
             chosen_option = random.choice(list(valid_options_passed_in))
 
-            all_options_folder_names = my_s3fs.ls(toplevel_dir)
+            all_options_folder_names = ALL_FOLDERS_OF_DATA
+
+            print("{}".format(all_options_folder_names))
 
             # to make only check folder names because aws s3fs.ls() returns whole path
             all_options_folder_names_only = [x.split('/')[-1] for x in all_options_folder_names]
 
-            chosen_option_quote = chosen_option+'_quotes'
-            chosen_option_lyrics = chosen_option+'_lyrics'
-            logging.debug('Chosen option: '+chosen_option)
-            logging.debug('valid options passed in: '+str(valid_options_passed_in))
+            chosen_option_quote = chosen_option + '_quotes'
+            chosen_option_lyrics = chosen_option + '_lyrics'
+            print('Chosen option: ' + chosen_option)
+            print('valid options passed in: ' + str(valid_options_passed_in))
             if chosen_option_lyrics in all_options_folder_names_only:
 
-                
-                the_file_lines,the_song,cat_folder = drill_down_and_get_file_and_song(toplevel_dir+chosen_option_lyrics)
+                the_file_lines, the_song, cat_folder = drill_down_and_get_file_and_song(
+                    toplevel_dir + chosen_option_lyrics)
 
             elif chosen_option in all_options_folder_names_only:
-                
-                the_file_lines,the_song,cat_folder = drill_down_and_get_file_and_song(toplevel_dir+chosen_option)
+
+                the_file_lines, the_song, cat_folder = drill_down_and_get_file_and_song(toplevel_dir + chosen_option)
 
             elif chosen_option_quote in all_options_folder_names_only:
-                
-                the_file_lines,the_song,cat_folder = drill_down_and_get_file_and_song(toplevel_dir+chosen_option_quote)
-                
 
+                the_file_lines, the_song, cat_folder = drill_down_and_get_file_and_song(
+                    toplevel_dir + chosen_option_quote)
+
+            print("all_options_folder_names_only: {}".format(all_options_folder_names_only))
+            print("chosen_option: {}".format(chosen_option))
+            print("chosen_option_lyrics: {}".format(chosen_option_lyrics))
+            print("chosen_option_quote: {}".format(chosen_option_quote))
             print("File Lines: {}".format(the_file_lines))
             print("Song: {}".format(the_song))
-            quote_or_lyric, author = piece_necessary_info_together(the_file_lines,the_song,wants_curses)
+            quote_or_lyric, author = piece_necessary_info_together(the_file_lines, the_song, wants_curses)
 
             if not author:
                 # if the author isnt determined in method above then it is the category folder name
@@ -169,20 +172,21 @@ def get_random_lyric(category_array=[]):
                 logging.debug('***** HIT SPLIT AUTHOR LOGIC*****')
                 # this depends on old naming convention of 'artistname _lyrics'
                 # if this returns nothing or blank string, use cat folder
-                if author in [' ','']:
+                if author in [' ', '']:
                     logging.debug('***** HIT BLANK AUTHOR LOGIC*****')
                     author = cat_folder
                     author = author.split('/')[-1]
 
                 the_song = the_song.split('/')[-1]
-            logging.debug('Returning author: '+author)
-            print('Cat folder was: '+cat_folder)
-            logging.debug('Returning quote or lyric: '+quote_or_lyric)
-            print('Returning song: '+the_song)
+            logging.debug('Returning author: ' + author)
+            print('Cat folder was: ' + cat_folder)
+            logging.debug('Returning quote or lyric: ' + quote_or_lyric)
+            print('Returning song: ' + the_song)
 
             return quote_or_lyric, the_song, author
 
-def drill_down_and_get_file_and_song(category_file_name_arg=None,wants_curses=True):
+
+def drill_down_and_get_file_and_song(category_file_name_arg=None, wants_curses=True):
     # the directory of the curent file
     #working_dir = os.path.dirname(os.path.abspath(__file__))
     # the data folder
@@ -192,8 +196,8 @@ def drill_down_and_get_file_and_song(category_file_name_arg=None,wants_curses=Tr
     # a random category within the folder
     if not category_file_name_arg:
         # ignore folders that have dots in them, not sure what this would be.
-        
-        sub_directories_of_data = my_s3fs.ls(toplevel_dir)
+
+        sub_directories_of_data = ALL_FOLDERS_OF_DATA
 
         # sub_directories_of_data = [sub_dir for sub_dir in os.listdir(data_folder_path) if '.' not in sub_dir]
         category_file_name = random.choice(sub_directories_of_data)
@@ -218,29 +222,29 @@ def drill_down_and_get_file_and_song(category_file_name_arg=None,wants_curses=Tr
     #full_path = path_to_chosen_category+os.sep+last_file_name
     full_path = last_file_name
     print("Reached full path: {}".format(full_path))
-    
-    with my_s3fs.open(full_path,'r',errors='ignore') as fl:
-        my_file_lines = fl.readlines()
 
+    with my_s3fs.open(full_path, 'r', errors='ignore') as fl:
+        my_file_lines = fl.readlines()
 
     potential_song = ''
     # if the file isnt a lyrics the text file will be saved with a quotes in line ending
     if 'quotes' in category_file_name.split('_'):
         potential_song = ''
-    elif 'lyrics' in category_file_name.split('_') or '_' not in category_file_name:
-        #cut out the .txt
-        potential_song = last_file_name[:-4].replace('_'," ")
+    else:  # 'lyrics' in category_file_name.split('_'):# or '_' not in category_file_name:
+        # cut out the .txt
+        potential_song = last_file_name[:-4].replace('_', " ")
 
-    return my_file_lines,potential_song,category_file_name
+    return my_file_lines, potential_song, category_file_name
 
-def are_bars_valid(bars_list,cursing_allowed=True):
+
+def are_bars_valid(bars_list, cursing_allowed=True):
     """
     Check to make sure the lines chosen don't have
     something like the artists name in brackets
     or [2x] or anything like that. We want a meaningful 4 lines
     Also check for the album info for the new type of folders
     """
-    check_if_bar_is_bad = lambda a:'[' in a or ']' in a or len(a) == 1 or '(' in a or ')' in a
+    check_if_bar_is_bad = lambda a: '[' in a or ']' in a or len(a) == 1 or '(' in a or ')' in a
     bar_validity_truth_array = [not check_if_bar_is_bad(bar) for bar in bars_list]
     if not cursing_allowed:
 
@@ -249,6 +253,7 @@ def are_bars_valid(bars_list,cursing_allowed=True):
         return (not curse) & all(bar_validity_truth_array)
 
     return all(bar_validity_truth_array)
+
 
 def is_valid_quote_author_combo(combo_list_quote_first_author_second):
     """
@@ -259,7 +264,8 @@ def is_valid_quote_author_combo(combo_list_quote_first_author_second):
     l = combo_list_quote_first_author_second
     return 'QUOTE' in l[0].split(':')[0].upper() and 'AUTHOR' in l[1].split(':')[0].upper()
 
-def piece_necessary_info_together(txt_file_lines,song,wants_curses=True):
+
+def piece_necessary_info_together(txt_file_lines, song, wants_curses=True):
     # if it is a song expect the bar format, where 2 lines make a bar
     if len(song) > 0:
         while(True):
@@ -267,25 +273,25 @@ def piece_necessary_info_together(txt_file_lines,song,wants_curses=True):
             try:
                 idx_of_album_info = txt_file_lines.index('ALBUM INFO\n')
             except ValueError:
-                #not every text file will have album info i.e. mixtapes and stuff
+                # not every text file will have album info i.e. mixtapes and stuff
                 idx_of_album_info = len(txt_file_lines)
 
             num_useful_lines = idx_of_album_info
             #print("Index of Last Useful Line: {}".format(num_useful_lines))
             #print("Number of lines in song: {}".format(len(txt_file_lines)))
             # up to the 4 before the end of useful lines so we can construct a whole bar
-            ind = random.choice(range(num_useful_lines-4))
+            ind = random.choice(range(num_useful_lines - 4))
             half_bar_1 = txt_file_lines[ind]
-            half_bar_2 = txt_file_lines[ind+1]
-            half_bar_3 = txt_file_lines[ind+2]
-            half_bar_4 = txt_file_lines[ind+3]
-            bars_all = [half_bar_1,half_bar_2,half_bar_3,half_bar_4]
+            half_bar_2 = txt_file_lines[ind + 1]
+            half_bar_3 = txt_file_lines[ind + 2]
+            half_bar_4 = txt_file_lines[ind + 3]
+            bars_all = [half_bar_1, half_bar_2, half_bar_3, half_bar_4]
             logging.info(bars_all)
-            if not are_bars_valid(bars_all,cursing_allowed=wants_curses):
+            if not are_bars_valid(bars_all, cursing_allowed=wants_curses):
                 continue
             else:
                 break
-        bar = half_bar_1+half_bar_2+half_bar_3+half_bar_4
+        bar = half_bar_1 + half_bar_2 + half_bar_3 + half_bar_4
         logging.info("Valid bar composed: {}".format(bar))
         author = None
         # author is left blank bc its a song, the author is in the parent directory name
@@ -306,10 +312,10 @@ def piece_necessary_info_together(txt_file_lines,song,wants_curses=True):
         This very clearly needs to be ported to a DB
         """
         while(True):
-            ind = random.choice(range(len(txt_file_lines)-1))
+            ind = random.choice(range(len(txt_file_lines) - 1))
             hopefully_quote = txt_file_lines[ind]
-            hopefully_author = txt_file_lines[ind+1]
-            hopeful_combo = [hopefully_quote,hopefully_author]
+            hopefully_author = txt_file_lines[ind + 1]
+            hopeful_combo = [hopefully_quote, hopefully_author]
             if not is_valid_quote_author_combo(hopeful_combo):
                 continue
             else:
@@ -318,8 +324,5 @@ def piece_necessary_info_together(txt_file_lines,song,wants_curses=True):
         quote = hopefully_quote[6:]
         # cut out the number of letters in the word "author " (peep the space)
         author = hopefully_author[7:]
-        logging.info("Valid quote and author found: {} - {}".format(quote,author))
+        logging.info("Valid quote and author found: {} - {}".format(quote, author))
         return quote, author
-
-
-
