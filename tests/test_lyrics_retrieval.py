2e9db734291b05c270ec10f34c8925c4a685db6a
tosin <tosin.sonuyi@gmail.com>
2019-01-23 00:55:39 -0500

    testing skeleton for internal funcs

---
+++ tests/test_lyrics_retrieval.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def test_necessary_info_pieced_together():
+    pass
+
+
+def test_is_valid_quote_author_combo():
+    pass
+
+
+def test_bar_validity_check_bridge_chorus_punctuation():
+    pass
+
+
+def test_bar_validity_check_cursing():
+    pass
